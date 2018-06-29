a632fae4c3d7e8e260b28f23b1b170595ccff1f0
patiencedaur <patiencedaur@gmail.com>
2018-06-29 15:42:52 +0300

    Fix comments in script.py

--- blog/management/commands/script.py
+++ blog/management/commands/script.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
 
     def handle(self, **options):
 
-        src = os.path.join(BASE_DIR, 'blog\comics') #исходная папка с комиксами
-        file_set = os.listdir(path=src) # нашли все комиксы в папке
+        src = os.path.join(BASE_DIR, 'blog\comics') #initial folder
+        file_set = os.listdir(path=src) # find all comics in folder
 
         print("Inserting comics...")
-        for file_name in file_set: # для каждого имени файла в папке
-            full_address = os.path.join(src, file_name) #полный исходный адрес файла
-            destination = os.path.join(BASE_DIR, 'media\comics', file_name) #полный конечный адрес файла
+        for file_name in file_set: # for every comic in initial folder
+            full_address = os.path.join(src, file_name) #full initial address
+            destination = os.path.join(BASE_DIR, 'media\comics', file_name) #full destination address
             print("Moving to " + destination)
-            shutil.copy2(full_address, destination) # переместить файлы в /media/comics
-            Comic.objects.create(pic=destination) # создать соответствующие объекты в базе данных
+            shutil.copy2(full_address, destination) # move files to /media/comics
+            Comic.objects.create(pic=destination) # create objects in database
