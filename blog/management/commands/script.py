47e5bb90e9b3e5b7127cee6014f75a21eedbaf1b
patiencedaur <patiencedaur@gmail.com>
2018-06-29 15:53:27 +0300

    Fix script.py again

--- blog/management/commands/script.py
+++ blog/management/commands/script.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
 
     def handle(self, **options):
 
-        src = os.path.join(BASE_DIR, 'blog\comics') #initial folder
+        src = os.path.join(BASE_DIR, 'blog', 'comics') #initial folder
         file_set = os.listdir(src) # find all comics in folder
 
         print("Inserting comics...")
         for file_name in file_set: # for every comic in initial folder
             full_address = os.path.join(src, file_name) #full initial address
-            destination = os.path.join(BASE_DIR, 'media\comics', file_name) #full destination address
+            destination = os.path.join(BASE_DIR, 'media', 'comics', file_name) #full destination address
             print("Moving to " + destination)
             shutil.copy2(full_address, destination) # move files to /media/comics
             Comic.objects.create(pic=destination) # create objects in database
