60b47c7b8b6d5e02eff591c2a365e8d7669b915d
patiencedaur <patiencedaur@gmail.com>
2018-08-01 19:20:52 +0300

    Adapt to mobile browsers

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,5 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Comic(models.Model):
     pic = models.ImageField(upload_to="comics")
-
-
-class Mobile(models.Model):
-    text = models.TextField()
